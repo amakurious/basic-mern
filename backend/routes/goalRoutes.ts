0694300b455e487c47726e3522a474d22d345fd0
Amakuri <40522150+amakurious@users.noreply.github.com>
2023-01-06 21:01:11 +0700

    Goals controller and routes setup

---
+++ backend/routes/goalRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+const {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(setGoals);
+router.route("/:id").put(updateGoals).delete(deleteGoals);
+
+module.exports = router;
